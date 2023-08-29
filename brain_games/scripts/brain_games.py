fede82025f85592aae908891a28e50f8cf426756
yadernaya <yadernaya@yahoo.com>
2023-08-29 17:13:22 +0300

    added import cli module

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,17 @@
-def main():
-    print('Welcome to the Brain Games!')
-if __name__ == '__main__':
-   main()
 
 #!/usr/bin/env python3
+
+from ..cli import welcome_user
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+def main():
+    greet()
+
+if __name__ == '__main__':
+    main()
+    
+welcome_user()
+
+
