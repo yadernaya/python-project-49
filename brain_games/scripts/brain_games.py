1b683c8778a7985f3da149bd1e20d6df971d7f7c
yadernaya <yadernaya@yahoo.com>
2023-08-29 22:37:57 +0300

    commit after flake8 installed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,18 +1,16 @@
-
 #!/usr/bin/env python3
 
 from ..cli import welcome_user
 
+
 def greet():
     print('Welcome to the Brain Games!')
+
 
 def main():
     greet()
     print(welcome_user())
 
+
 if __name__ == '__main__':
     main()
-    
-
-
-
