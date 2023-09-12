5fa57e7667cf96edd03ae5dc9c925347f213202a
yadernaya <yadernaya@yahoo.com>
2023-09-12 23:04:40 +0300

    fixed calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-from brain_games.games.calc import greet
-from brain_games.games.calc import rules_of_game
 from brain_games.games.calc import calc_game
 
 
 def main():
-    greet()
-    rules_of_game()
     calc_game()
 
 
